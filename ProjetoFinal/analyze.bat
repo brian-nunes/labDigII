7e4d35041b6bd36ae9abde3b5b8c841953381b4e
Brian Nunes <brian.nunes@usp.br>
2021-11-15 00:53:23 -0300

    Importação de entidades, e analyze.bat

---
+++ ProjetoFinal/analyze.bat
@@ -0,0 +1,25 @@
+ghdl -a contadorg_m.vhd
+ghdl -a contador_bcd_4digitos.vhd
+ghdl -a deslocador_n.vhd
+ghdl -a edge_detector.vhd
+ghdl -a gerador_pulso.vhd
+ghdl -a registrador_deslocamento_n.vhd
+ghdl -a registrador_n.vhd
+
+ghdl -a controle_servo_3.vhd
+
+ghdl -a rx_serial_tick_uc.vhd
+ghdl -a rx_serial_8N2_fd.vhd
+ghdl -a rx_serial_8N2.vhd
+ghdl -a tx_serial_tick_uc.vhd
+ghdl -a tx_serial_8N2_fd.vhd
+ghdl -a tx_serial_8N2.vhd
+ghdl -a uart_8N2.vhd
+
+ghdl -a interface_hcsr04_uc.vhd
+ghdl -a interface_hcsr04_fd.vhd
+ghdl -a interface_hcsr04.vhd
+
+ghdl -a gaiola_uc.vhd
+ghdl -a gaiola_fd.vhd
+ghdl -a gaiola.vhd
--- ProjetoFinal/gaiola_fd.vhd
+++ ProjetoFinal/gaiola_fd.vhd
@@ -12,9 +12,60 @@ ENTITY gaiola_fd IS
 END ENTITY;
 
 ARCHITECTURE arch_gaiola_fd OF gaiola_fd IS
-    -- precisa de uart para transmitir distância
-    -- precisa de interface hcsr04 para medir distância
-    -- precisa de controle_servo_3 para controlar servo
-    -- precisa de edge_detector para levar pulso do sinal de transmite e mede para suas entidades
+    COMPONENT uart_8N2
+        PORT (
+            clock : IN STD_LOGIC;
+            reset : IN STD_LOGIC;
+            transmite_dado : IN STD_LOGIC;
+            dados_ascii : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+            dado_serial : IN STD_LOGIC;
+            recebe_dado : IN STD_LOGIC;
+            saida_serial : OUT STD_LOGIC;
+            pronto_tx : OUT STD_LOGIC;
+            dado_recebido_rx : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+            tem_dado : OUT STD_LOGIC;
+            pronto_rx : OUT STD_LOGIC;
+            db_transmite_dado : OUT STD_LOGIC;
+            db_saida_serial : OUT STD_LOGIC;
+            db_estado_tx : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+            db_recebe_dado : OUT STD_LOGIC;
+            db_dado_serial : OUT STD_LOGIC;
+            db_estado_rx : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
+        );
+    END COMPONENT;
+
+    COMPONENT interface_hcsr04
+        PORT (
+            clock, reset : IN STD_LOGIC;
+            medir : IN STD_LOGIC;
+            echo : IN STD_LOGIC;
+            trigger : OUT STD_LOGIC;
+            medida : OUT STD_LOGIC_VECTOR(11 DOWNTO 0);
+            pronto : OUT STD_LOGIC;
+            db_estado : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
+        );
+    END COMPONENT;
+
+    COMPONENT controle_servo_3
+        PORT (
+            clock : IN STD_LOGIC;
+            reset : IN STD_LOGIC;
+            posicao : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+            pwm : OUT STD_LOGIC;
+            db_reset : OUT STD_LOGIC;
+            db_pwm : OUT STD_LOGIC;
+            db_posicao : OUT STD_LOGIC_VECTOR(2 DOWNTO 0)
+        );
+    END COMPONENT;
+
+    COMPONENT edge_detector
+        PORT (
+            clk : IN STD_LOGIC;
+            signal_in : IN STD_LOGIC;
+            output : OUT STD_LOGIC
+        );
+    END COMPONENT;
+
 BEGIN
+
 END ARCHITECTURE;
